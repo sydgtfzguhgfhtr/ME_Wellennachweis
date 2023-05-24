a9cd8f762efa899e0f1682bf6efa24102bb8ec20
sydgtfzguhgfhtr <schulzn2004@gmail.com>
2023-05-24 19:23:00 +0200

    Formzahl für Absätze und Ringnuten

---
+++ Wellennachweis_Funktionen.py
@@ -0,0 +1,35 @@
+# Importe:
+from Werkstoffdaten import Werkstoff_liste
+import numpy as np
+
+
+# Benötigte gegebene Größen:
+Durchmesser = 50
+Art_der_Schwächung = "Wellenabsatz"
+
+
+# Formzahl für Absätze, und Ringnuten:
+def Formzahl(Absatzart, Belastung, grosser_Durchmesser, kleiner_Durchmesser, Radius):
+    t = (grosser_Durchmesser-kleiner_Durchmesser)/2
+    if Absatzart == "umlaufende Rundnut":
+        match Belastung:
+            case "Zug/Druck":
+                alpha = Formzahl_Unterfunktion_Formel(0.22,1.37,0,0,kleiner_Durchmesser,grosser_Durchmesser,Radius,t)
+            case "Biegung":
+                alpha = Formzahl_Unterfunktion_Formel(0.2,2.75,0,0,kleiner_Durchmesser,grosser_Durchmesser,Radius,t)
+            case "Torsion":
+                alpha = Formzahl_Unterfunktion_Formel(0.7,10.3,0,0,kleiner_Durchmesser,grosser_Durchmesser,Radius,t)
+    elif Absatzart == "Absatz":
+        match Belastung:
+            case "Zug/Druck":
+                alpha = Formzahl_Unterfunktion_Formel(0.62,3.5,0,0,kleiner_Durchmesser,grosser_Durchmesser,Radius,t)
+            case "Biegung":
+                alpha = Formzahl_Unterfunktion_Formel(0.62,5.8,0.2,3,kleiner_Durchmesser,grosser_Durchmesser,Radius,t)
+            case "Torsion":
+                alpha = Formzahl_Unterfunktion_Formel(3.4,19,1,2,kleiner_Durchmesser,grosser_Durchmesser,Radius,t)
+    return alpha
+
+# nicht einzeln benutzen; nur für Funktion Formzahl!!
+def Formzahl_Unterfunktion_Formel(A,B,C,z,d,D,r,t):
+    alpha = 1+ 1/(np.sqrt(A*r/t+2*B*r/d*(1+2*r/d)**2+C*(r/t)**z*d/D))
+    return alpha
