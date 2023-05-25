fb2551fd3ee9463aa106148135c65e9ef1e93573
sydgtfzguhgfhtr <schulzn2004@gmail.com>
2023-05-25 18:49:08 +0200

    Kerbwirkungszahl für Passfedern, Keilwellen, Kerbzahnwellen und Zahnwellen

--- Wellennachweis_Funktionen.py
+++ Wellennachweis_Funktionen.py
@@ -4,6 +4,7 @@ Funktionssammlung zur Durchführung eines Wellennachweis.
 
 # Importe:
 import numpy as np
+import math
 from Klassen import Werkstoff
 
 Werkstoff.aus_csv_laden() # Werkstoffdaten laden
@@ -36,3 +37,31 @@ def Formzahl(Absatzart, Belastung, grosser_Durchmesser, kleiner_Durchmesser, Rad
             case "Torsion":
                 alpha = Formzahl_Unterfunktion_Formel(3.4,19,1,2,kleiner_Durchmesser,grosser_Durchmesser,Radius,t)
     return alpha
+
+def Kerbwirkungszahl(Art, sigma_B):
+    """
+    returns: (Kerbwirkungszahl für Biegung, Kerbwirkungszahl für Torsion)
+    """
+    if Art == "eine Passfeder":
+        beta_sigma = 3*(sigma_B/1000)**0.38
+        beta_tau = 0.56*beta_sigma+0.1
+    elif Art == "zwei Passfedern":
+        beta_sigma = (3*(sigma_B/1000)**0.38)*1.15
+        beta_tau = 0.56*beta_sigma+0.1
+    elif Art == "Pressverband":
+        beta_sigma = 2.7*(sigma_B/1000)**0.43
+        beta_tau = 0.65*beta_sigma
+    elif Art == "Keilwelle" or Art == "Kerbzahnwelle" or Art == "Zahnwelle":
+        beta_tau_stern = math.e**(4.2*10**(-7)*(sigma_B)**2)
+        match Art:
+            case "Keilwelle":
+                beta_sigma = 1+0.45*(beta_tau_stern-1)
+                beta_tau = beta_tau_stern
+            case "Kerbzahnwelle":
+                beta_sigma = 1+0.65*(beta_tau_stern-1)
+                beta_tau = beta_tau_stern
+            case "Zahnwelle":
+                beta_sigma = 1+0.49*(beta_tau_stern-1)
+                beta_tau = 1+0.75*(beta_tau_stern-1)
+    Ergebnis = (beta_sigma, beta_tau)
+    return Ergebnis
