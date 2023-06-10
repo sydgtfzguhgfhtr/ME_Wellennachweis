88397b14ef3bcf0dfcc95a5fee2a47fedc23d14f
KwentiN <97750754+KwentiN-ui@users.noreply.github.com>
2023-06-10 14:04:07 +0200

    Klasse Welle weiterentwickelt

--- Klassen.py
+++ Klassen.py
@@ -52,12 +52,36 @@ class Welle:
         self.geometrie = []
         self.z_daten = []
         self.r_daten = []
+        self.belastungen = []
+    
+    def set_Kraft(self,betrag,typ:str,z=0,r=0,phi=0):
+        """Legt eine Krafteinleitung an der Welle fest.
+        Der Typ kann "radial","tangential" oder "axial" sein.
+        Radialkräfte nach innen sind positiv.
+        z: Z-Position (längs) wo die Kraft angreift
+        r: Entfernung der Kraft von der Drehachse der Welle.
+        phi: Winkel der Krafteinleitung in Grad. Kräfte von oben haben phi=0
+        """
+        phi = np.deg2rad(phi)
+        if typ[0].casefold()=="r":
+            kraft = (betrag,z,r,phi,betrag*np.sin(phi),betrag*np.cos(phi),0)
+        elif typ[0].casefold()=="t":
+            kraft = (betrag,z,r,phi,betrag*np.cos(phi),betrag*np.sin(phi),0)
+        elif typ[0].casefold()=="a":
+            kraft = (betrag,z,r,phi,0,0,betrag)
+        else:
+            raise ValueError("Krafttyp wurde nicht erkannt. Erlaubt sind 'radial','tangential' und 'axial'.")
+        self.belastungen.append(kraft)
+        # Lagerkräfte berechnen und in Belastungen aufnehmen
+        #self.belastungen.insert(0, () )
+
     def set_geometrie(self,punkte:list):
         """
         Definiert die Wellengeometrie als Liste aus Punkten in der Form [[z1,r1],[z2,r2],...]
         """
         self.geometrie = punkte
         self.z_daten,self.r_daten = zip(*self.geometrie)
+
     def radius(self,z):
         """Gibt Radius der Welle an Stelle z aus. Alle Längen werden in `mm` angegeben"""
         for i,zr in enumerate(self.geometrie):
@@ -67,14 +91,16 @@ class Welle:
             if z<=z2:
                 m = (z - z1) / (z2 - z1)
                 return r1 + m * (r2 - r1)
-        else:
-            return 0
-
+    def r(self,z):
+        """Gibt Radius der Welle an Stelle z aus. Alle Längen werden in `mm` angegeben"""
+        return self.radius(z)
     def d(self,z):
         """Gibt Durchmesser der Welle an Stelle z aus. Alle Längen werden in `mm` angegeben"""
         return 2*self.radius(z)
-
-    def plot(self):
+    def durchmesser(self,z):
+        """Gibt Durchmesser der Welle an Stelle z aus. Alle Längen werden in `mm` angegeben"""
+        return self.d(z)
+    def plot(self,biegemomente=False):
         """Stellt die Welle dar."""
         zrange = np.arange(0,max(self.z_daten),0.1)
         rrange = np.array(tuple(map(self.radius,zrange)))
@@ -101,7 +127,8 @@ if __name__ == "__main__":
         [100,40],
         [130,40],
         [130,25],
-        [160,25],
+        [200,25],
     ])
-    print(test.geometrie)
-    test.plot()
+    test.set_Kraft(1,"r",100,test.radius(100),0)
+    #test.plot()
+    print(test.belastungen)
