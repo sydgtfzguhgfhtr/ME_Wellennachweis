0354589ffbc2368ab61b68bbf360618a9a873f07
sydgtfzguhgfhtr <schulzn2004@gmail.com>
2023-06-29 16:17:30 +0200

    ID

--- Lager/ID.py
+++ Lager/ID.py
@@ -1,5 +1,15 @@
 import pandas as pd
+import numpy as np
 
-Daten = pd.read_csv(r"C:\Users\Nadine\Documents\Studium\Studium\1234567890\ME_Wellen\ME_Wellennachweis\Lager\einreihige_Rillenkugellager.csv")
+Daten = pd.read_csv(r"Lager\Einreihige_Zylinderrollenlager.csv")
 
-Daten.to_csv(r"C:\Users\Nadine\Documents\Studium\Studium\1234567890\ME_Wellen\ME_Wellennachweis\Lager\einreihige_Rillenkugellager.csv", sep=',')
+Nummern = []
+
+for i in range(791,1347):
+    Nummern.append(i)
+
+neu = pd.concat([Daten,{"ID":[i for i in range(791,1347)]}],axis=1)
+
+#print(neu)
+
+#Daten.to_csv(r"Lager\Einreihige_Zylinderrollenlager.csv", sep=',')
